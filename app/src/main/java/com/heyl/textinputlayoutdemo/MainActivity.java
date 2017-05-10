1a191ecd008a783c6267d526136397c33d6014c2
He YongLiang <heyl2804@gmail.com>
2017-05-10 11:02:02 +0800

    第二次

--- app/src/main/java/com/heyl/textinputlayoutdemo/MainActivity.java
+++ app/src/main/java/com/heyl/textinputlayoutdemo/MainActivity.java
@@ -25,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        tilOne.setHint("用户名");
-        tilTwo.setHint("密码");
+        tilOne.setHint("helloWord_1");
+        tilTwo.setHint("helloWord_2");
 
     }
 
